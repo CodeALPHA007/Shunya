c01a212b3cc5bbeffaad76822acd2db7174a900a
CodeALPHA007 <71808602+CodeALPHA007@users.noreply.github.com>
2024-03-15 03:20:46 +0530

    Update Planet.py

--- Scripts/Planet.py
+++ Scripts/Planet.py
@@ -1,4 +1,5 @@
 from ursina import *
+from PIL import Image
 
 PI= 3.14
 class Planet:
@@ -11,6 +12,7 @@ class Planet:
             self.obj=Entity(model='sphere', texture=texture_path)   
             self.obj.rotation_z+=tilt
             self.obj.scale=(radius,radius,radius)
+            self.autorotation=True
 
         def pos(self, x: float, y: float, z: float):
             self.obj.position=Vec3(x,y,z)
@@ -28,21 +30,29 @@ class Planet:
 if __name__=='__main__':
     app= Ursina()
     planet=Planet()
-    jupiter=planet.Create_entity(model='sphere', texture_path=r"..\Assets\jupiter.png",radius=3,tilt=45)
+    jupiter=planet.Create_entity(model='sphere', texture_path=r"..\Assets\jupiter.jpg",radius=3,tilt=45)
     jupiter.pos(0,0,0)
     
+    def input(key):
+        if key=='r':
+            jupiter.autorotation=not(jupiter.autorotation)
+        elif key in ['w','up arrow','a','left arrow','s','down arrow','d','right arrow'] :
+            jupiter.autorotation=False
+         
     def update():
-        y_val_l_r= 100 * ( held_keys['d'] or held_keys['right arrow'] ) * time.dt
-        y_val_r_l= 100 * ( held_keys['a'] or held_keys['left arrow'] ) * time.dt
-        x_val_d_u= 100 * ( held_keys['w'] or held_keys['up arrow'] ) * time.dt
-        x_val_u_d= 100 * ( held_keys['s'] or held_keys['down arrow'] ) * time.dt
+        if not(jupiter.autorotation):
+            y_val_l_r=100*(held_keys['d'] or held_keys['right arrow']) * time.dt
+            y_val_r_l=100*(held_keys['a'] or held_keys['left arrow']) * time.dt
+            x_val_d_u=100*(held_keys['w'] or held_keys['up arrow']) * time.dt
+            x_val_u_d=100*(held_keys['s'] or held_keys['down arrow']) * time.dt
+           
+            jupiter.rotation(left_to_right_rotation=True,axis='y',rotation_value=y_val_l_r)
+            jupiter.rotation(left_to_right_rotation=False,axis='y',rotation_value=y_val_r_l)
+            jupiter.rotation(left_to_right_rotation=True,axis='x',rotation_value=x_val_u_d)
+            jupiter.rotation(left_to_right_rotation=False,axis='x',rotation_value=x_val_d_u)
+            
+        else:
+            jupiter.rotation(left_to_right_rotation=True,axis='y',rotation_value=1)
 
-
-        jupiter.rotation(left_to_right_rotation=True,axis='y',rotation_value=y_val_l_r)
-        jupiter.rotation(left_to_right_rotation=False,axis='y',rotation_value=y_val_r_l)
-        jupiter.rotation(left_to_right_rotation=True,axis='x',rotation_value=x_val_u_d)
-        jupiter.rotation(left_to_right_rotation=False,axis='x',rotation_value=x_val_d_u)
-        
-    
     app.run()
     
