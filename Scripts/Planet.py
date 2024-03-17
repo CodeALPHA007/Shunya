ba5a23138a0e2db5c3159ab79e7c7ae2bc3769ff
RupayanMandal <71808602+CodeALPHA007@users.noreply.github.com>
2024-03-17 22:05:34 +0530

    Update Planet.py

--- Scripts/Planet.py
+++ Scripts/Planet.py
@@ -8,24 +8,50 @@ class Planet:
         
         
     class Create_entity :
-        def __init__(self, model: str, texture_path: str,radius: float, tilt: float= 0.0 ) :
-            self.obj=Entity(model='sphere', texture=texture_path)   
-            self.obj.rotation_z+=tilt
+        def __init__(self,texture_path: str, model: str='sphere', radius: float=3, tilt: float= 0.0 ) :  
+            self.obj=Entity(model=model, texture=texture_path)   
+            self.pos(0,0,0)
+            self.tilt(status=True, val= tilt)
             self.obj.scale=(radius,radius,radius)
-            self.autorotation=True
+            self._auto_rotation=True
+            self._tilt=tilt
+            self._auto_rotation_text='<yellow>\nAutorotation: {}</yellow>'
+        
+        def reset_pos_rot(self):
+            self.pos(0,0,0)
+            self.obj.rotation=Vec3(0,0,0)
 
+        def tilt(self,status: bool= False, val : float= 0):
+            self.obj.rotation=Vec3(0,0,0)
+            if not(status):
+                self.obj.rotation_z=0
+            else:
+                self.obj.rotation_z=val
+                
         def pos(self, x: float, y: float, z: float):
             self.obj.position=Vec3(x,y,z)
 
+        def get_text(self) -> str:
+            if self._auto_rotation:
+                temp_text=self._auto_rotation_text.format(self._auto_rotation)
+            else:
+                temp_text=self._auto_rotation_text.format(self._auto_rotation)
+            return temp_text
+        
+        
         def rotation(self,left_to_right_rotation: bool =True, axis : str ='y' , rotation_value: int = 10):
-            absolute_rotation_value=abs(rotation_value)
+            
             if left_to_right_rotation:
                 rotation_value*=-1
             if axis=='z':
                 self.obj.rotate(Vec3(0,0,rotation_value))   
             elif axis=='y':
                 self.obj.rotate(Vec3(0,rotation_value,0))
+        
+        def toggle_auto_rotation(self):
+            self._auto_rotation=not(self._auto_rotation)
 
+            
 
 if __name__=='__main__':
     app= Ursina()
@@ -35,12 +61,18 @@ if __name__=='__main__':
     
     def input(key):
         if key=='r':
-            jupiter.autorotation=not(jupiter.autorotation)
+            if jupiter.auto_rotation==False:
+                #error
+                #jupiter.transform = Func(Transform,scale=(2, 2, 2))
+                #not error
+                jupiter.scale=Vec3(3,3,3)
+            jupiter.auto_rotation=not(jupiter.autorotation)
         elif key in ['w','up arrow','a','left arrow','s','down arrow','d','right arrow'] :
-            jupiter.autorotation=False
-         
+            jupiter.auto_rotation=False
+            jupiter.rotate_z=0
     def update():
-        if not(jupiter.autorotation):
+        if not(jupiter._auto_rotation):
+           
             y_val_l_r=100*(held_keys['d'] or held_keys['right arrow']) * time.dt
             y_val_r_l=100*(held_keys['a'] or held_keys['left arrow']) * time.dt
             z_val_d_u=100*(held_keys['w'] or held_keys['up arrow']) * time.dt
@@ -53,6 +85,6 @@ if __name__=='__main__':
             
         else:
             jupiter.rotation(left_to_right_rotation=True,axis='y',rotation_value=1)
-
+            jupiter.pos(0,0,0)
     app.run()
     
